9e85288928282310dd69749ffb9ff1770b21e5af
Denis Strizhkin <strdenis02@gmail.com>
2022-05-28 15:39:35 +0300

    added emms to emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,1 +1,14 @@
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(emms lammps-mode openwith dired-x company magit vterm spacemacs-theme lsp-mode lsp-modea go-mode which-key use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
